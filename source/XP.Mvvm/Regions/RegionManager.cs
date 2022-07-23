2208524ccd205d966b95e035bb37cf9db4f9b9cb
Jan Schubert <dev@schubert-jan.de>
2022-07-23 09:11:00 +0200

    Fix RegionManager.

--- source/XP.Mvvm/Regions/RegionManager.cs
+++ source/XP.Mvvm/Regions/RegionManager.cs
@@ -14,9 +14,9 @@ namespace XP.Mvvm.Regions
     private static void RegionChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
       if (d is TabControl tabControl)
-        _regions.Add((string)e.NewValue, new TabRegion(tabControl));
+        _regions[(string)e.NewValue] = new TabRegion(tabControl);
       else
-        _regions.Add((string)e.NewValue, new SingleContentRegion((ContentControl) d));
+        _regions[(string)e.NewValue] = new SingleContentRegion((ContentControl) d);
     }
 
     public static void SetRegion(DependencyObject element, string value)
