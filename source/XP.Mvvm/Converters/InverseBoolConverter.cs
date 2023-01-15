48f35b589bd3e617a74fdbbdd58c59310d682367
Jan Schubert <dev@schubert-jan.de>
2023-01-15 12:31:15 +0100

    Add InverseBoolConverter.

---
+++ source/XP.Mvvm/Converters/InverseBoolConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Markup;
+
+namespace XP.Mvvm.Converters;
+
+public class InverseBoolConverter : MarkupExtension, IValueConverter
+{
+    protected override object ProvideValue(IXamlServiceProvider serviceProvider)
+    {
+        return this;
+    }
+
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is bool boolValue)
+        {
+            return !boolValue;
+        }
+
+        return false;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        if (value is bool boolValue)
+        {
+            return !boolValue;
+        }
+
+        return false;
+    }
+}
