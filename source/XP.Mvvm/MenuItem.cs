d62623068427fb6533bd122fc4bcedc771fb90fa
Jan Schubert <dev@schubert-jan.de>
2025-04-05 20:22:49 +0200

    Update MenuItem.cs

--- source/XP.Mvvm/MenuItem.cs
+++ source/XP.Mvvm/MenuItem.cs
@@ -7,9 +7,11 @@ namespace XP.Mvvm
   {
     public string Title { get; set; }
     public DelegateCommand Command { get; set; }
-    public Func<bool> IsCheckedFunc { get; set; }
     public MenuItem Parent { get; set; }
     public int Order { get; set; }
     public ObservableCollection<MenuItem> Items { get; } = new();
+
+    public bool IsCheckable { get; set; }
+    public bool IsChecked { get; set; }
   }
 }
